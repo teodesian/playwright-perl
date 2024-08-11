67467610fd69677f0d836361b785773f286eea86
George Baugh <george@troglodyne.net>
2024-08-11 21:06:49 +0000

    un-break generate_api_json.sh

--- generate_api_json.sh
+++ generate_api_json.sh
@@ -8,6 +8,6 @@ node utils/doclint/generateApiJson.js > ../playwright-perl/api.json
 popd
 cp playwright_server bin/playwright_server
 API="$(<api.json)"
-sed -i '' -e '/%REPLACEME%/r api.json' -e 's/%REPLACEME%//g' bin/playwright_server
+sed -i -e '/%REPLACEME%/r api.json' -e 's/%REPLACEME%//g' bin/playwright_server
 # Make which work on windows
 cp bin/playwright_server bin/playwright_server.bat
