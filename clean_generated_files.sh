804db0fac46fd937ba67475419191a7fb5746102
Keith Carangelo <mail@kcaran.com>
2024-08-13 11:22:52 -0400

    Add sed backup file to fix macos build, make successful commands quieter

--- clean_generated_files.sh
+++ clean_generated_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm api.json; true
-rm bin/playwright_server; true
+rm -f api.json; true
+rm -f bin/playwright_server; true
 for module in $(git ls-files -o --exclude-standard lib/Playwright)
 do
-    rm $module; true
+    rm -f $module; true
 done
